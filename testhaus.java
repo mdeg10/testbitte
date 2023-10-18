4d4ad7a0637d6a6c9d39719bf23d56385edef73c
mdeg10 <michaild.projekte@gmail.com>
2023-10-18 11:54:48 +0200

    nue version

--- testhaus.java
+++ testhaus.java
@@ -1,1 +1,9 @@
-
+public class testhaus{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] b= {1,2,3,9,5,6,7,8};
+		//System.out.println(teileUndHerrsche(b,80,0,b.length-1));
+		System.out.println(4);
+		//System.out.println(pos3(b,9,0,b.length-1));
+	}
+}
